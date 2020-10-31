2a84eb539ff5e84a832f6c9a351ab37b32ba715e
lagofelipe <lagofelipe@gmail.com>
2020-10-31 10:34:17 -0500

    controllers

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,15 @@
 class MissionsController < ApplicationController
+
+ def index
+  @missions = Mission.all
+ end
+
+ def show
+  @mission = Mission.find(params[:id]) 
+ end
+
+
+
+
+
 end
--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,2 +1,46 @@
 class ScientistsController < ApplicationController
+
+def index
+ @scientists = Scientist.all
+end
+
+def show
+ @scientist = Scientist.find(params[:id])
+end
+
+def new
+ @scientist = Scientist.new
+end
+
+def create
+ @new_scientist = Scientist.create(scientist_params)
+
+ redirect_to scientist_path(@new_scientist)
+end
+
+def edit
+@scientist = Scientist.find(params[:id])
+end
+
+def update
+
+end
+
+def destroy
+@scientist.destroy
+
+#after action redirect? Unsure about this as a regular workflow
+redirect_to scientists_path
+end
+
+
+
+
+
+private
+
+def scientist_params
+params.require(:scientist).permit(:name, :field_of_study)
+end
+
 end
